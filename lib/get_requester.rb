49400fa90eadfbb2357398a24a6a7ee7e2067295
Ray Alva <rayalva407@gmail.com>
2020-04-11 03:16:03 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,7 +7,7 @@ class GetRequester
   def initialize(url)
   end
     
-  def get_response_body(url = ''https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'')
+  def get_response_body(url)
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     response.body
