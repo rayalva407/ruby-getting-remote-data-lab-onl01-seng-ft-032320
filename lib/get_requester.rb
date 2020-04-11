dbc4e15e726ccf45e098ae8a91f8e8b25821e8ec
Ray Alva <rayalva407@gmail.com>
2020-04-11 03:30:24 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -15,7 +15,10 @@ class GetRequester
   end
   
   def parse_json
-    
+    programs = JSON.parse(self.get_programs)
+    programs.collect do |program|
+      program["agency"]  
+    end
   end
   
 end
