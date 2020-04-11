088ce6dc93cfe85ffcecde9b5ac2e9af97fb06f2
Ray Alva <rayalva407@gmail.com>
2020-04-11 03:06:49 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,5 +3,11 @@ require 'open-uri'
 require 'json'
 
 class GetRequester
-  
+  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+ 
+  def get_programs
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
 end
